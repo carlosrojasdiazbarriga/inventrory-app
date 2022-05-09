f2fcb180bcb42edf44d6a2f3779990de7a4022f1
koryto <carlos.rojas.diaz.barriga@gmail.com>
2022-05-08 23:00:12 -0500

    camara trasera

--- src/Components/QrLector.js
+++ src/Components/QrLector.js
@@ -5,7 +5,7 @@ import {QrReader} from "react-qr-reader";
 export const QrLector = (props) => {
 
   
-  const [selected, setSelected] = useState("environment");
+  const [selected, setSelected] = useState();
   const [data, setData] = useState('No result');
 
   props.func(data);//enviar la data del qr
@@ -22,19 +22,32 @@ export const QrLector = (props) => {
     marginLeft:'25%'
   };
 
+  const handleSelected=(e)=> {
+    if(e.target.value==='environment'){
+      // console.log('env')
+      setSelected ({
+        facingMode: 'environment'
+      })
+    }else{
+      setSelected ({
+        facingMode: 'user'
+      })
+    }
+  }
+
   return (
     <div className="App card-body ">
       <h3>Escaneá el codigo QR</h3>
       <h4>Apuntá con la camara al codigo, se detectará automáticamente</h4>
       <select 
-        onChange={(e) => setSelected(e.target.value)}
+        onChange={handleSelected}
         className="form-select"
         >
         <option value={"environment"}>Cámara trasera</option>
         <option value={"user"}>Cámara delantera</option>
       </select>
         <QrReader
-        facingMode={selected}
+        constraints={selected}
         containerStyle = {previewStyle}
         onResult={(result, error) => {
           if (!!result) {
